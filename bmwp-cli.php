0256390462d28efa99378d75831e15c78e942561
Yakovos Frountas <iakovosfrountas@gmail.com>
2023-08-16 16:30:38 +0300

    Update bmwp-cli.php

--- bmwp-cli.php
+++ bmwp-cli.php
@@ -36,6 +36,42 @@ class BMWP_WPCLI {
             WP_CLI::error( "✖️ Failed to archive the website." );
         }
     }
+
+    /**
+     * Upload an image from a URL and attach it to a WordPress post.
+     *
+     * @param int $post_id The ID of the post to which the image should be attached.
+     * @return int $attach_id The attachment ID of the uploaded image.
+     */
+    public static function upload_and_attach_image($post_id) {
+        $image_url = 'https://picsum.photos/1200/800';
+        $upload_dir = wp_upload_dir();
+        $image_data = file_get_contents($image_url);
+        $filename = wp_unique_filename($upload_dir['path'], 'featured-' . $post_id . '.jpg');
+
+        if(wp_mkdir_p($upload_dir['path'])) {
+            $file = $upload_dir['path'] . '/' . $filename;
+        } else {
+            $file = $upload_dir['basedir'] . '/' . $filename;
+        }
+
+        file_put_contents($file, $image_data);
+
+        $wp_filetype = wp_check_filetype($filename, null);
+        $attachment = array(
+            'post_mime_type' => $wp_filetype['type'],
+            'post_title' => sanitize_file_name($filename),
+            'post_content' => '',
+            'post_status' => 'inherit'
+        );
+
+        $attach_id = wp_insert_attachment($attachment, $file, $post_id);
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
+        $attach_data = wp_generate_attachment_metadata($attach_id, $file);
+        wp_update_attachment_metadata($attach_id, $attach_data);
+
+        return $attach_id;
+    }
 }
 
 class BMWP_Generate_Posts {
@@ -45,7 +81,7 @@ class BMWP_Generate_Posts {
      *
      * ## EXAMPLES
      *
-     *     wp bmwp-posts
+     *     wp bmwp-import-posts
      */
 
     /**
@@ -135,28 +171,7 @@ class BMWP_Generate_Posts {
                 'post_category'=> array( $actor_cat_id )
             ) );
 
-            // Download and set a random featured image
-            $image_url = 'https://picsum.photos/1200/800';
-            $upload_dir = wp_upload_dir();
-            $image_data = file_get_contents($image_url);
-            $filename = wp_unique_filename( $upload_dir['path'], 'featured-' . $post_id . '.jpg' ); // changed filename generation
-            if(wp_mkdir_p($upload_dir['path'])) 
-                $file = $upload_dir['path'] . '/' . $filename;
-            else
-                $file = $upload_dir['basedir'] . '/' . $filename;
-            file_put_contents($file, $image_data);
-
-            $wp_filetype = wp_check_filetype($filename, null );
-            $attachment = array(
-                'post_mime_type' => $wp_filetype['type'],
-                'post_title' => sanitize_file_name($filename),
-                'post_content' => '',
-                'post_status' => 'inherit'
-            );
-            $attach_id = wp_insert_attachment( $attachment, $file, $post_id );
-            require_once(ABSPATH . 'wp-admin/includes/image.php');
-            $attach_data = wp_generate_attachment_metadata( $attach_id, $file );
-            wp_update_attachment_metadata( $attach_id, $attach_data );
+            $attach_id = BMWP_WPCLI::upload_and_attach_image($post_id);
             set_post_thumbnail( $post_id, $attach_id );
 
             WP_CLI::log(WP_CLI::colorize("%cPost titled '{$movie}' created successfully!%n"));
@@ -169,6 +184,206 @@ class BMWP_Generate_Posts {
     }
 
 }
+
+class BMWP_Generate_Woo_Products {
+
+    /**
+     * Adds sample products to a WooCommerce store.
+     *
+     * ## EXAMPLES
+     *
+     *     wp bmwp-import-products
+     */
+    public function __invoke() {
+        if ( is_plugin_active('woocommerce/woocommerce.php')) {
+            WP_CLI::line('WooCommerce is active. Proceeding with product import...');
+
+            $this->import_simple_products();
+            $this->import_variable_products();
+
+            WP_CLI::success('Finished importing products.');
+        } else {
+            WP_CLI::warning('WooCommerce is not active. Please activate it first.');
+        }
+    }
+
+    private function import_simple_products() {
+        // Sample adjectives and nouns for generating product names
+        $adjectives = array('Glossy', 'Matte', 'Stylish', 'Elegant', 'Modern', 'Vintage', 'Sleek', 'Rustic', 'Bold', 'Subtle');
+        $nouns = array('Lamp', 'Chair', 'Table', 'Rug', 'Shelf', 'Clock', 'Mirror', 'Vase', 'Mug', 'Bowl');
+    
+        for ($i = 0; $i < 15; $i++) {
+            $product_name = $adjectives[array_rand($adjectives)] . ' ' . $nouns[array_rand($nouns)];
+    
+            $product = new WC_Product();
+            $product->set_name($product_name);
+            $product->set_status("publish");
+            $product->set_catalog_visibility('visible');
+            $product->set_description('Sample description for ' . $product_name);
+            $product->set_regular_price(rand(10, 200)); // Random price between 10 and 200
+            
+            $product->save();
+
+            $attach_id = BMWP_WPCLI::upload_and_attach_image($product->get_id());
+            set_post_thumbnail($product->get_id(), $attach_id);
+    
+        }
+    }
+
+    private function import_variable_products() {
+        $sizes = array('Small', 'Medium', 'Large', 'XLarge', 'XXLarge');
+        $adjectives = array('Glossy', 'Matte', 'Stylish', 'Elegant', 'Modern', 'Vintage', 'Sleek', 'Rustic', 'Bold', 'Subtle');
+        $nouns = array('T-Shirt', 'Jacket', 'Jeans', 'Dress', 'Blouse', 'Pants', 'Sweater', 'Shorts', 'Coat', 'Scarf');
+    
+        for ($i = 0; $i < 5; $i++) {
+            $product_name = $adjectives[array_rand($adjectives)] . ' ' . $nouns[array_rand($nouns)];
+    
+            // Creating the variable product
+            $product = new WC_Product_Variable();
+            $product->set_name($product_name);
+            $product->set_status("publish");
+            $product->set_catalog_visibility('visible');
+            $product->set_description('Sample description for ' . $product_name);
+            
+            $product->save();
+
+            $attach_id = BMWP_WPCLI::upload_and_attach_image($product->get_id());
+            set_post_thumbnail($product->get_id(), $attach_id);
+    
+            // Adding sizes as product attribute
+            $attributes = array();
+            $attribute = new WC_Product_Attribute();
+            $attribute->set_id(0);
+            $attribute->set_name('Size');
+            $attribute->set_options($sizes);
+            $attribute->set_position(0);
+            $attribute->set_visible(1);
+            $attribute->set_variation(1);
+            $attributes[] = $attribute;
+            $product->set_attributes($attributes);
+            $product->save();
+    
+            // Adding product variations (like size Small with a specific price)
+            foreach ($sizes as $size) {
+                $variation = new WC_Product_Variation();
+                $variation->set_parent_id($product->get_id());
+                $variation->set_attributes(array('Size' => $size));
+                $variation->set_status("publish");
+                $variation->set_regular_price(rand(10, 100)); // Random price between 10 and 100 for each size
+                $variation->save();
+            }
+        }
+    }
+}
+
+class BMWP_Site_Settings_Command {
+
+    /**
+     * Check and update WPForms settings.
+     *
+     * ## EXAMPLES
+     *
+     *     wp bmwp-fix-plugin-settings
+     */
+    public function __invoke() {
+
+        // Flags to track if we made changes
+        $settings_updated = false;
+        
+        // Check if WPForms is active
+        if (is_plugin_active('wpforms/wpforms.php') || is_plugin_active('wpforms-lite/wpforms.php')) {
+            $wpforms_settings = get_option('wpforms_settings');
+
+            // Check if settings exist and are in array format
+            if (!$wpforms_settings || !is_array($wpforms_settings)) {
+                WP_CLI::error("WPForms settings not found or in unexpected format.");
+                return;
+            }
+ 
+            if (!isset($wpforms_settings['hide-announcements']) || $wpforms_settings['hide-announcements'] === false) {
+                $wpforms_settings['hide-announcements'] = '1';
+                $settings_updated = true;
+            }
+            
+            // Check 'email-summaries-disable' setting and set it if not set or if false
+            if (!isset($wpforms_settings['email-summaries-disable']) || $wpforms_settings['email-summaries-disable'] === false) {
+                $wpforms_settings['email-summaries-disable'] = '1';
+                $settings_updated = true;
+            }
+
+            if ($settings_updated) {
+                update_option('wpforms_settings', $wpforms_settings);
+                WP_CLI::success("WPForms settings updated successfully.");
+            }else {
+                WP_CLI::log("All WPForms settings are correct.");
+            }
+        } else {
+            WP_CLI::warning("WPForms plugin is not activated.");
+        }
+
+        // Check if WP Mail SMTP is active
+        if (is_plugin_active('wp-mail-smtp/wp_mail_smtp.php')) {
+            $wp_mail_smtp_settings = get_option('wp_mail_smtp');
+
+            if (!$wp_mail_smtp_settings || !is_array($wp_mail_smtp_settings)) {
+                WP_CLI::error("WP Mail SMTP settings not found or in unexpected format.");
+                return;
+            }
+
+            if ( !isset($wp_mail_smtp_settings['general']['dashboard_widget_hidden']) || $wp_mail_smtp_settings['general']['dashboard_widget_hidden'] === false ) {
+                $wp_mail_smtp_settings['general']['dashboard_widget_hidden'] = '1';
+                $settings_updated = true;
+            }
+    
+            if (!isset($wp_mail_smtp_settings['general']['summary_report_email_disabled']) || $wp_mail_smtp_settings['general']['summary_report_email_disabled'] === false) {
+                $wp_mail_smtp_settings['general']['summary_report_email_disabled'] = '1';
+                $settings_updated = true;
+            }
+    
+            if (!isset($wp_mail_smtp_settings['general']['am_notifications_hidden']) || $wp_mail_smtp_settings['general']['am_notifications_hidden'] === false) {
+                $wp_mail_smtp_settings['general']['am_notifications_hidden'] = '1';
+                $settings_updated = true;
+            }
+
+            if ($settings_updated) {
+                update_option('wp_mail_smtp', $wp_mail_smtp_settings);
+                WP_CLI::success("WP Mail SMTP settings updated successfully.");
+            }
+        } else {
+            WP_CLI::warning("WP Mail SMTP plugin is not activated.");
+        }
+    
+
+        // Check if Perfmatters is active
+        if (is_plugin_active('perfmatters/perfmatters.php')) {
+
+            $perfmatters_options = get_option('perfmatters_options');
+
+            if (!isset($perfmatters_options['login_url']) || $perfmatters_options['login_url'] === "") {
+                $perfmatters_options['login_url'] = 'user-login';
+                $settings_updated = true;
+            }
+    
+            if (!isset($perfmatters_options['login_url_behavior']) || $perfmatters_options['login_url_behavior'] === "" ) {
+                $perfmatters_options['login_url_behavior'] = '404';
+                $settings_updated = true;
+            }
+       
+            if ($settings_updated) {
+                update_option('perfmatters_options', $perfmatters_options);
+                WP_CLI::success("Perfmatters options updated successfully.");
+            }
+        } else {
+            WP_CLI::warning("Perfmatters plugin is not activated.");
+        }
+
+        if ($settings_updated) {
+            WP_CLI::runcommand('cache flush');
+        }
+       
+    }
+}
+
 
 class BMWP_Site_Onboard_Command {
 
@@ -314,8 +529,10 @@ class BMWP_Site_Export_Command {
 }
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-    WP_CLI::add_command( 'bmwp-posts',   'BMWP_Generate_Posts' );
-    WP_CLI::add_command( 'bmwp-onboard', 'BMWP_Site_Onboard_Command' );
-    WP_CLI::add_command( 'bmwp-offload', 'BMWP_Site_Offload_Command' );
-    WP_CLI::add_command( 'bmwp-export',  'BMWP_Site_Export_Command' );
+    WP_CLI::add_command( 'bmwp-import-posts',        'BMWP_Generate_Posts' );
+    WP_CLI::add_command( 'bmwp-import-products',     'BMWP_Generate_Woo_Products' );
+    WP_CLI::add_command( 'bmwp-fix-plugin-settings', 'BMWP_Site_Settings_Command' );
+    WP_CLI::add_command( 'bmwp-onboard-settings',    'BMWP_Site_Onboard_Command' );
+    WP_CLI::add_command( 'bmwp-offload-site',        'BMWP_Site_Offload_Command' );
+    WP_CLI::add_command( 'bmwp-export-site',         'BMWP_Site_Export_Command' );
 }
